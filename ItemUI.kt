893d8c088c7df139a76e4ca23bbe76134346ae24
Moises Vera <42410348+cheroga@users.noreply.github.com>
2026-04-06 11:38:21 -0300

    Delete ItemUI.kt

--- ItemUI.kt
+++
@@ -1,8 +0,0 @@
-package com.moisnostudio.jerutiapp.ui
-
-import com.moisnostudio.jerutiapp.model.Sample
-
-sealed class ItemUI {
-    data class Titulo(val nombre: String) : ItemUI()
-    data class Canal(val datos: Sample) : ItemUI()
-}
